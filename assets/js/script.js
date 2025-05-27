0c40a27454e9f649f7ef683de7313af1edb2c464
Camilla Rodrigues <camillarodriguesfp2@gmail.com>
2025-05-27 19:08:58 -0300

    modo dark finalizado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,19 @@
 const mode = document.getElementById("mode-icon");
+const body = document.body;
 
 mode.addEventListener("click", () => {
+
     if (mode.classList.contains("fa-moon")) {
         mode.classList.remove("fa-moon");
         mode.classList.add("fa-sun");
+
+        body.classList.add("dark");
     }
     else {
         mode.classList.remove("fa-sun");
         mode.classList.add("fa-moon");
+
+        body.classList.remove("dark");
     }
 });
+
